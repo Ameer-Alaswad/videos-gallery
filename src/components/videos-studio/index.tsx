73179f186b51936eb8cc93d90c7f44165d0cd420
mouhamad <mouhamadx92x@gmail.com>
2023-12-31 18:20:12 +0100

    testing

--- src/components/videos-studio/index.tsx
+++ src/components/videos-studio/index.tsx
@@ -7,60 +7,66 @@ import { studioVideosData } from "../../assets";
 
 export default function StandardImageList() {
     const [open, setOpen] = useState<boolean>(false);
-    const [videoLink, setVideoLink] = useState<string>("")
+    const [videoLink, setVideoLink] = useState<string>("");
 
     const handleOpen = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
         const targetElement = event.target as HTMLElement;
-        const firstChild = targetElement.firstChild as HTMLElement
-        setVideoLink(firstChild.id)
+        const firstChild = targetElement.firstChild as HTMLElement;
+        setVideoLink(firstChild.id);
         setOpen(true);
-    }
+    };
     return (
         <Box
-            sx={ {
+            sx={{
                 width: "100%",
                 height: "100%",
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
-            } }
+            }}
         >
-            <ImageList cols={ 3 } rowHeight={ 230 }>
-                { studioVideosData.map((item) => (
+            <ImageList cols={3} rowHeight={231}>
+                {studioVideosData.map((item) => (
                     <ImageListItem
-                        onClick={ (event) => handleOpen(event) }
-                        sx={ {
-                            ":hover": { transform: "scale(1.5)", zIndex: "10000" },
+                        onClick={(event) => handleOpen(event)}
+                        sx={{
+                            ":hover": {
+                                transform: "scale(1.5)",
+                                zIndex: "10000",
+                            },
                             display: "flex",
                             alignItems: "center",
                             justifyContent: "center",
-
-                        } }
-                        key={ item.video }>
+                        }}
+                        key={item.video}
+                    >
                         <video
-                            style={ {
+                            style={{
                                 width: "250px",
                                 height: "200px",
                                 transition: "transform 0.3s",
-                                objectFit: "cover"
-                            } }
+                                objectFit: "cover",
+                            }}
                             loop
                             muted
                             autoPlay
                         >
                             <source
-                                id={ item.title }
-                                src={ `${item.video}` }
+                                id={item.title}
+                                src={`${item.video}`}
                                 type="video/mp4"
                             />
                             Your browser does not support the video tag.
                         </video>
                     </ImageListItem>
-                )) }
-                <BasicModal videoLink={ videoLink } open={ open } setOpen={ setOpen } />
+                ))}
+                <BasicModal
+                    videoLink={videoLink}
+                    open={open}
+                    setOpen={setOpen}
+                />
             </ImageList>
         </Box>
     );
 }
-
