17c845ad8abe1a99c66ed593ada82c94dc17c4c7
mouhamad <mouhamadx92x@gmail.com>
2024-01-03 00:58:19 +0100

    refactoring

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,34 +1,13 @@
-import { Divider, Box } from "@mui/material";
+import { Divider, Box, Typography } from "@mui/material";
+import { footerMainBoxStyle, dividerStyle, textStyle } from "./style";
 
 const Footer: React.FC = () => {
     return (
-        <Box
-            sx={{
-                width: "100%",
-                backgroundColor: "#fff",
-                paddingBottom: "50px",
-                marginBottom: 0,
-                borderTop: "1px solid '#ddd'",
-            }}
-        >
-            <Divider
-                style={{
-                    width: "70%",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    color: "#000",
-                    boxShadow: "0 1px 3px rgba(0, 0, 0, 0.125)",
-                }}
-            />
-            <p
-                style={{
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    color: "#000",
-                }}
-            >
+        <Box sx={footerMainBoxStyle}>
+            <Divider style={dividerStyle} />
+            <Typography sx={textStyle}>
                 Copyright © 2024 ABO ALSHABAB
-            </p>
+            </Typography>
         </Box>
     );
 };
---
+++ src/components/footer/style.tsx
@@ -0,0 +1,21 @@
+export const footerMainBoxStyle = {
+    width: "100%",
+    backgroundColor: "#fff",
+    paddingBottom: "50px",
+    marginBottom: 0,
+    borderTop: "1px solid '#ddd'",
+};
+
+export const dividerStyle = {
+    width: "70%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    color: "#000",
+    boxShadow: "0 1px 3px rgba(0, 0, 0, 0.125)",
+};
+
+export const textStyle = {
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#000",
+};
