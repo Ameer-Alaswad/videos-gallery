3411b1bd472ffca0b036d93027e8a95cd3945259
mouhamad <mouhamadx92x@gmail.com>
2024-01-06 00:44:05 +0100

    adding keys and id to the .env file

--- src/components/header/contact-us-popover/contact-us-form/index.tsx
+++ src/components/header/contact-us-popover/contact-us-form/index.tsx
@@ -3,6 +3,10 @@ import emailjs, { EmailJSResponseStatus } from "emailjs-com";
 import { Box, Input } from "@mui/material";
 import { CONTACT_ME } from "../../../../assets/text";
 
+const serviceId = import.meta.env.VITE_SERVICE_ID as string;
+const templateId = import.meta.env.VITE_TEMPLATE_ID as string;
+const publicKey = import.meta.env.VITE_PUBLIC_KEY as string;
+
 const ContactUsForm = () => {
     const form = useRef<HTMLFormElement>(null);
     const sendEmail = (e: FormEvent) => {
@@ -10,12 +14,7 @@ const ContactUsForm = () => {
 
         if (form.current) {
             emailjs
-                .sendForm(
-                    "YOUR_SERVICE_ID",
-                    "YOUR_TEMPLATE_ID",
-                    form.current,
-                    "YOUR_PUBLIC_KEY"
-                )
+                .sendForm(serviceId, templateId, form.current, publicKey)
                 .then(
                     (result: EmailJSResponseStatus) => {
                         console.log(result.text);
