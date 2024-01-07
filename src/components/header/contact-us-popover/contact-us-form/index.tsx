c5300eabb0b94bdd841b223c684928ae273fba8e
mouhamad <mouhamadx92x@gmail.com>
2024-01-07 16:43:14 +0100

    refactoring

--- src/components/header/contact-us-popover/contact-us-form/index.tsx
+++ src/components/header/contact-us-popover/contact-us-form/index.tsx
@@ -2,6 +2,7 @@ import { useRef, FormEvent } from "react";
 import emailjs, { EmailJSResponseStatus } from "emailjs-com";
 import { Box, Button, InputLabel, TextareaAutosize } from "@mui/material";
 import { MESSAGE, SEND } from "../../../../assets/text";
+import { buttonStyles, textAreaStyles } from "./styles";
 
 const serviceId = import.meta.env.VITE_SERVICE_ID as string;
 const templateId = import.meta.env.VITE_TEMPLATE_ID as string;
@@ -9,6 +10,7 @@ const publicKey = import.meta.env.VITE_PUBLIC_KEY as string;
 
 const ContactUsForm = () => {
     const form = useRef<HTMLFormElement>(null);
+
     const sendEmail = (e: FormEvent) => {
         e.preventDefault();
 
@@ -43,24 +45,13 @@ const ContactUsForm = () => {
                     id="my-textarea"
                     name="message"
                     minRows={3}
-                    style={{
-                        color: "white",
-                        width: "90%",
-                        height: "100px",
-                        marginLeft: "20px",
-                        borderRadius: "8px",
-                    }}
+                    style={textAreaStyles}
                 />
                 <Button
                     type="submit"
                     variant="text"
                     color="inherit"
-                    style={{
-                        border: "none",
-                        background: "none",
-                        color: "black",
-                        marginLeft: "43%",
-                    }}
+                    style={buttonStyles}
                 >
                     {SEND}
                 </Button>
---
+++ src/components/header/contact-us-popover/contact-us-form/styles.tsx
@@ -0,0 +1,15 @@
+export const textAreaStyles = {
+    width: "90%",
+    height: "100px",
+    marginLeft: "20px",
+    borderRadius: "8px",
+    color: "black",
+    padding: "5px",
+};
+
+export const buttonStyles = {
+    border: "none",
+    background: "none",
+    color: "black",
+    marginLeft: "43%",
+};
