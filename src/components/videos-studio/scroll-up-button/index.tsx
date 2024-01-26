4f6bf5573785f95916a6beb52c5c74a22d51818a
mouhamad <mouhamadx92x@gmail.com>
2024-01-26 15:05:36 +0100

    scroll up smoothly

--- src/components/videos-studio/scroll-up-button/index.tsx
+++ src/components/videos-studio/scroll-up-button/index.tsx
@@ -25,8 +25,10 @@ const ScrollToTopButton = () => {
     }, []);
 
     const scrollToTop = () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     };
 
     const StyledButton = styled("button")({
